b2f7b703110dcc6d628a48c2558a2035a94216cd
Michael <mjryan110@gmail.com>
2021-02-16 21:30:57 -0600

    updating

--- src/client/index.js
+++ src/client/index.js
@@ -1,11 +1,6 @@
 import { checkForName } from './js/checkForName'
 import { handleSubmit } from './js/formHandler'
 
-import './styles/resets.scss'
-import './styles/base.scss'
-import './styles/footer.scss'
-import './styles/form.scss'
-import './styles/formresults.scss'
-import './styles/header.scss'
+import './styles/style.scss'
 
 export { checkForName, handleSubmit};
