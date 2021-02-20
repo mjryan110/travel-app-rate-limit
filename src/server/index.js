1bf0a9cddc32a28b96322ea643d89a7ae6e9c8e3
Michael <mjryan110@gmail.com>
2021-02-20 10:14:25 -0600

    updating

--- src/server/index.js
+++ src/server/index.js
@@ -18,53 +18,42 @@ app.use(bodyParser.json());
 //Initialize the main project folder
 app.use(express.static('dist'))
 
-//API
-const apiKey = process.env.API_KEY
-const baseURL = 'https://api.meaningcloud.com/sentiment-2.1?'
-console.log(`Your API key is ${process.env.API_KEY}`);
-
 const port = 8081;
 
 app.listen(port, function () {
     console.log(`Running on localhost: ${port}`)
 })
 
-//Geonames API
-const geonames_apiKey = process.env.GEONAMES_API_KEY
-const geonames_baseURL = 'https://api.meaningcloud.com/sentiment-2.1?'
-console.log(`Your API key is ${process.env.GEONAMES_API_KEY}`);
-
-//Weatherbit API
-const weatherbit_apiKey = process.env.WEATHERBIT_API_KEY
-const weatherbit_baseURL = 'https://api.weatherbit.io/v2.0/forecast/daily?'
-console.log(`Your API key is ${process.env.WEATHERBIT_API_KEY}`);
-
-//Pixabay API
-const pixabay_apiKey = process.env.PIXABAY_API_KEY
-const pixabay_baseURL = 'https://pixabay.com/api/?'
-console.log(`Your API key is ${process.env.PIXABAY_API_KEY}`);
-
-//Send to client
+// Get
 app.get('/', function (req, res) {
     res.sendFile('dist/index.html')
     })
 
-//Post route
+//// Geonames API ////
+const GEONAMES_USER_NAME = process.env.GEONAMES_USER_NAME
+const geonames_baseURL = 'api.geonames.org/search?'
+console.log(`Your API key is ${process.env.GEONAMES_USER_NAME}`);
+
+// Post Route
 app.post('/api', async function(req, res) {
-    userURL = req.body.url;
-    console.log(`You entered: ${userURL}`);
-    const apiURL = `${baseURL}key=${apiKey}&url=${userURL}&lang=en`
+    city = req.body.city;
+    console.log(`You entered: ${city}`);
+    const geonamesINFO = `${geonames_baseURL}q=${city}&username=${GEONAMES_USER_NAME}&maxRows=1&lang=en`
 
-    const response = await fetch(apiURL)
-    const sentimentData = await response.json()
-    console.log(sentimentData)
-    res.send(sentimentData)
-    const projectData = {
-        agreement : sentimentData.agreement,
-        confidence: sentimentData.confidence,
-        subjectivity: sentimentData.subjectivity
-    }
-
-    res.send(projectData);
-
+    const response = await fetch(geonamesINFO)
+    const city_lat_long = await response.json()
+    console.log(city_lat_long)
+    res.send(city_lat_long)
 })
+
+
+
+//// Weatherbit API ////
+const weatherbit_apiKey = process.env.WEATHERBIT_API_KEY
+const weatherbit_baseURL = 'https://api.weatherbit.io/v2.0/forecast/daily?'
+console.log(`Your API key is ${process.env.WEATHERBIT_API_KEY}`);
+
+//// Pixabay API //// 
+const pixabay_apiKey = process.env.PIXABAY_API_KEY
+const pixabay_baseURL = 'https://pixabay.com/api/?'
+console.log(`Your API key is ${process.env.PIXABAY_API_KEY}`);
