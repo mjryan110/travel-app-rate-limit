8f1bcad27313f60e671b3e193a506707ebfe9e05
Michael Ryan <61295989+mjryan110@users.noreply.github.com>
2021-12-20 14:10:29 -0600

    Update index.js

--- src/server/index.js
+++ src/server/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const fetch = require('node-fetch');
 const dotenv = require('dotenv');
+const redis = require('./redis-client.js')
 dotenv.config();
 
 // start up app
@@ -34,15 +35,44 @@ const GEONAMES_USER_NAME = "mjryan110"
 const geonames_baseURL = 'http://api.geonames.org/searchJSON?'
 
 // Post Route
-app.post('/city', async function(req, res) {
+app.post('/city', async (req, res) => {
+    const ip = req.headers['x-forwarded-for'] || req.socket.remoteAddress
+
+    const requests = await redis.incr(ip)
+
+    let ttl
+    if(requests === 1) {
+        await redis.expire(ip, 60)
+        ttl = 60
+    } else {
+        ttl = await redis.ttl(ip)
+    }
+    
+    if(requests > 20) {
+        return res.status(503).json({
+            response: 'error',
+            callsInAMinute: requests,
+            ttl
+        })
+    }
+    
+    console.log('Number of requests made so far', requests)
+
     destcity = req.body.city;
     console.log(`You entered: ${destcity}`);
     const geonamesINFO = `${geonames_baseURL}q=${destcity}&username=${GEONAMES_USER_NAME}&maxRows=1&lang=en`
-    console.log(geonamesINFO)
+    //console.log(geonamesINFO)
 
     const geonamesFetch = await fetch(geonamesINFO)
     const city_lat_long = await geonamesFetch.json()
+    console.log(city_lat_long)
     res.send(city_lat_long)
+
+ //   return res.json({
+ //       response: 'ok',
+ //       callsInAMinute: requests,
+ //       ttl
+ //   })
 })
 
 
@@ -58,6 +88,7 @@ app.post('/weather', async function (req, res) {
 
     const weatherbitFetch = await fetch(weatherbitINFO)
     const weatherbitFetchJSON = await weatherbitFetch.json()
+    console.log(weatherbitFetchJSON)
     res.send(weatherbitFetchJSON)
 })
 
